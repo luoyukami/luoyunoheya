df0248dfc2032560e1e3de7ab1fdbc62bd372164
luoyukami <875435729@qq.com>
2016-10-26 16:15:34 +0800

    listen port test

---
+++ src/web/app.js
@@ -0,0 +1,37 @@
+'use strict'
+
+var Koa = require('koa')
+var sha1 = require('sha1')
+var config = {
+  wechat:{
+    appId:'wxb5515478cb25bcc5',
+    appSecret:'a3ee2c9a7c7083e05e13af1a10db5fe7',
+    token:'luoyukaminoheya9zhangjin'
+  }
+}
+
+var app = new Koa()
+
+app.use(function *(next) {
+  console.log(this.query)
+
+  var token = wechat.token
+  var signature = this.query.signature
+  var nonce = this.query.nonce
+  var timestamp = this.query.timestamp
+  var ecostr = this.query.ecostr
+
+
+  var str = [token,timestamp,nonce].sort().join('')
+  var sha = sha1(str)
+
+  if(sha === signature){
+    this.body = ecostr + ''
+  }
+  else{
+    this.body = 'wrong'
+  }
+})
+
+app.listen(1234)
+console.log('Listening:1234')
